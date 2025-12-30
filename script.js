0e0be38de1ddc83643ba6b29f2004705dc486125
madhukayal88-glitch <madhukayal88@gmail.com>
2025-12-30 19:47:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function makeid(l) {
-  // write your code here
+ function makeid(l) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    
+    for (let i = 0; i < l; i++) {
+        // Generate a random index based on the length of the characters string
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        
+        // Append the character at that index to our result string
+        result += characters.charAt(randomIndex);
+    }
+    
+    return result;
+ }
 }
 
 // Do not change the code below.
